c9466a203ce3a151af3f5fa9a3a47b75ff2aca02
CC <55222885+chloec222@users.noreply.github.com>
2020-02-11 19:04:25 -0500

    updated route address

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var app = express();
 var PORT = process.env.PORT || 8080;
 
 // For serving of static CSS
-app.use(express.static(__dirname + "app/public/style.css"));
+app.use(express.static(__dirname + "/app/public/style.css"));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -17,8 +17,8 @@ app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 // API and HTML routes
-require("app/routing/apiRoutes.js")(app);
-require("app/routing/htmlRoutes.js")(app);
+require("/app/routing/apiRoutes.js")(app);
+require("/app/routing/htmlRoutes.js")(app);
 
 app.listen(PORT, function() {
 	console.log("App listening on PORT: " + PORT);
